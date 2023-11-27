c519b6dfc66ea0daf60429f4f7daee4c32f0a218
Mahesh Prasad Pisharody <67485594+mahesh349@users.noreply.github.com>
2023-11-26 21:35:52 -0500

    Delete src directory

--- src/routes/roomRoutes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-const router = Router();
-
-router.route('/').get(async (req, res) => {
-    res.render('room/addRoom', {title:"Add Room"});
-  });
-
-export default router;
