84441e4e211100818b64f71500f689f368eb12d7
rivdsilva8 <rivaldodsilva8@gmail.com>
2023-11-29 04:49:22 -0500

    admin routes/handlebars setup , login/register handlebars added,  mongodb config added

--- helpers.js
+++ helpers.js
@@ -1,2 +1,9 @@
 // mahesh, yuzhi, kaushik: Bookings, room both routes and data functions
+
 // sushmita, rivaldo: account, feedback, gallery both routes and data functions
+
+export function isAdmin(object) {
+  if (object.isAdmin == true) {
+    return true;
+  } else return false;
+}
--- routes/admin/gallery.js
+++ routes/admin/gallery.js
@@ -1,2 +1,15 @@
 // admin here can add, delete, and see all the photos he want to keep in the gallery page:
 // GET all, POST, DELETE
+
+import { Router } from "express";
+const router = Router();
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("./Admin/adminGallery/adminGallery", {
+      title: "admin gallery manipulation",
+    });
+  } catch (e) {}
+});
+
+export default router;
