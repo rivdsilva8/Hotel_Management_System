7b4769fa148b1e985eececae073ff24457ae73a9
rivdsilva8 <rivaldodsilva8@gmail.com>
2023-11-30 02:14:28 -0500

    Added .post  for admin/feedbackroute to create a new feedback

--- routes/RoutesIndex.js
+++ routes/RoutesIndex.js
@@ -1,8 +1,5 @@
 import adminRoutes from "./admin/index.js";
-<<<<<<< Updated upstream
-=======
 import usersRoutes from "./user_routes.js";
->>>>>>> Stashed changes
 
 const constructorMethod = (app) => {
   app.get("/", (req, res) => {
@@ -15,26 +12,21 @@ const constructorMethod = (app) => {
 
   app.get("/register", (req, res) => {
     res.render("./login/UserCreate");
-<<<<<<< Updated upstream
   });
-=======
-  });*/
-  app.use("/", usersRoutes);
->>>>>>> Stashed changes
 
   //admin routes
   // TODO: middleware to check if user is actually an admin
 
   app.use("/admin", adminRoutes);
 
-    app.use("*", (req, res) => {
-      res.render("error", {
-        title: "Error",
-        code: 404,
-        hasError: true,
-        error: "page not found",
-      });
+  app.use("*", (req, res) => {
+    res.render("error", {
+      title: "Error",
+      code: 404,
+      hasError: true,
+      error: "page not found",
     });
+  });
 };
 
 export default constructorMethod;
