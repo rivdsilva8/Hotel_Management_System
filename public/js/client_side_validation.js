d5f7cabb60db1d06eb1a7709584023242907f255
sbahala <112838583+sbahala@users.noreply.github.com>
2023-12-16 16:36:21 -0500

    ClientSideValidationandHtmlErrorFix

--- public/js/client_side_validation.js
+++ public/js/client_side_validation.js
@@ -4,15 +4,37 @@ document.addEventListener("DOMContentLoaded", function(){
     const registerForm = document.querySelector('form[action="/register"]');
     const resetForm = document.querySelector('form[action="/reset"]');
     const errorDiv = document.getElementById('error-container');
+    const userEditForm = document.querySelector('#userEditForm');
+    const adminAccountSearch = document.querySelector('#adminAccountSearch');
+    const adminCreateForm = document.querySelector('#adminCreateForm');
 
-    if(resetForm){
-        resetForm.addEventListener('submit', function(event){
+    if(adminCreateForm){
+        adminCreateForm.addEventListener('submit',function(event){
             let errorMessage =[];
-            const email = document.getElementById('email').value;
+
+            const firstName = document.getElementById('firstNameInput').value;
+            const lastName = document.getElementById('lastNameInput').value;
+            const userEmail = document.getElementById('email').value;
+            const phone = document.getElementById('phone').value;
             const password = document.getElementById('password').value;
-            const confirmPassword = document.getElementById('confirmPassword').value;
-            if(!validateEmail(email)){
+            const confirmPassword = document.getElementById('cpassword').value;
+            if(!firstName || firstName.length <2 || firstName.length>25 || !/^[A-Za-z]+$/.test(firstName)){
+                errorMessage.push('First Name must be between 2 to 25 characters and must contain only letters.');
+            }
+            if(!lastName || lastName.length <2 || lastName.length>25 || !/^[A-Za-z]+$/.test(lastName)){
+                errorMessage.push('First Name must be between 2 to 25 characters and must contain only letters.');
+            }
+            if(!validateEmail(userEmail)){
                 errorMessage.push('Please enter a valid email address.');
+            }
+            const [prefix,domain]=userEmail.split("@");
+            if(!/^([a-zA-Z0-9]+([_\.-]?[a-zA-Z0-9]+)*)$/.test(prefix) || !/^([a-zA-Z0-9-]+)+(\.[a-zA-Z]{2,})+$/.test(domain)) 
+            {
+            errorMessage.push(' Email Address given does not  have a valid prefix or domain'); 
+            }
+            const phoneValidationResult = validatePhone(phone);
+            if(phoneValidationResult !== true){
+                errorMessage.push(phoneValidationResult);
             }
             if(!password){
                 errorMessage.push('Please enter your password');               
@@ -28,7 +50,141 @@ document.addEventListener("DOMContentLoaded", function(){
                     errorMessage.push('Password must be a valid string with no spaces and should be at least of 8 characters long.');
                 }
                 if(!/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[^A-Za-z0-9]/.test(password)){
-                    errorMessage.push('Password must be contain at least one uppercase character, one number and one special character.');
+                    errorMessage.push('Password must contain at least one uppercase character, one number and one special character.');
+                }
+                if(typeof(confirmPassword) !== 'string' || confirmPassword.includes(' ')|| confirmPassword.length<8){
+                    errorMessage.push('ConfirmPassword must be a valid string with no spaces and should be at least of 8 characters long.');
+                }
+                if(!/[A-Z]/.test(confirmPassword) || !/[0-9]/.test(confirmPassword) || !/[^A-Za-z0-9]/.test(confirmPassword)){
+                    errorMessage.push('ConfirmPassword must be contain at least one uppercase character, one number and one special character.');
+                }
+            }
+            if(errorMessage.length>0){
+                event.preventDefault();
+                errorDiv.innerHTML = errorMessage.join('<br>');
+                errorDiv.style.display ='block';
+            }
+
+
+        });
+    }
+
+    document.body.addEventListener('submit',function(event){
+        if(event.target.classList.contains('admin-edit-form')){
+            let errorMessage =[];
+            const targetForm = event.target;
+            const accountId = targetForm.dataset.accountId;
+            const firstName = document.getElementById('firstNameInput-' + accountId).value;
+            const lastName = document.getElementById('lastNameInput-' + accountId).value;
+            const userEmail = document.getElementById('email-' + accountId).value;
+            const phone = document.getElementById('phone-' + accountId).value;
+            if(!firstName || firstName.length <2 || firstName.length>25 || !/^[A-Za-z]+$/.test(firstName)){
+                errorMessage.push('First Name must be between 2 to 25 characters and must contain only letters.');
+            }
+            if(!lastName || lastName.length <2 || lastName.length>25 || !/^[A-Za-z]+$/.test(lastName)){
+                errorMessage.push('First Name must be between 2 to 25 characters and must contain only letters.');
+            }
+            if(!validateEmail(userEmail)){
+                errorMessage.push('Please enter a valid email address.');
+            }
+            const [prefix,domain]=userEmail.split("@");
+            if(!/^([a-zA-Z0-9]+([_\.-]?[a-zA-Z0-9]+)*)$/.test(prefix) || !/^([a-zA-Z0-9-]+)+(\.[a-zA-Z]{2,})+$/.test(domain)) 
+            {
+            errorMessage.push(' Email Address given does not  have a valid prefix or domain'); 
+            }
+            const phoneValidationResult = validatePhone(phone);
+            if(phoneValidationResult !== true){
+                errorMessage.push(phoneValidationResult);
+            }
+            if(errorMessage.length>0){
+                event.preventDefault();
+                errorDiv.innerHTML = errorMessage.join('<br>');
+                errorDiv.style.display ='block';
+            }
+        }
+    });
+
+
+    if(adminAccountSearch){
+        adminAccountSearch.addEventListener('submit',function(event){
+            let errorMessage =[];
+            const firstName = document.getElementById('searchFName').value;
+            if(!firstName || firstName.length <2 || firstName.length>25 || !/^[A-Za-z]+$/.test(firstName)){
+                errorMessage.push('First Name must be between 2 to 25 characters and must contain only letters.');
+            }
+
+            if(errorMessage.length>0){
+                event.preventDefault();
+                errorDiv.innerHTML = errorMessage.join('<br>');
+                errorDiv.style.display ='block';
+            }
+        })
+    }
+
+    if(userEditForm){
+        userEditForm.addEventListener('submit', function(event){
+            let errorMessage =[];
+            const firstName = document.getElementById('firstNameInput').value;
+            const lastName = document.getElementById('lastNameInput').value;
+            const userEmail = document.getElementById('email').value;
+            const phone = document.getElementById('phone').value;
+
+            if(!firstName || firstName.length <2 || firstName.length>25 || !/^[A-Za-z]+$/.test(firstName)){
+                errorMessage.push('First Name must be between 2 to 25 characters and must contain only letters.');
+            }
+            if(!lastName || lastName.length <2 || lastName.length>25 || !/^[A-Za-z]+$/.test(lastName)){
+                errorMessage.push('Last Name must be between 2 to 25 characters and must contain only letters.');
+            }
+
+            if(!validateEmail(userEmail)){
+                errorMessage.push('Please enter a valid email address.');
+            }
+            const [prefix,domain]=userEmail.split("@");
+            if(!/^([a-zA-Z0-9]+([_\.-]?[a-zA-Z0-9]+)*)$/.test(prefix) || !/^([a-zA-Z0-9-]+)+(\.[a-zA-Z]{2,})+$/.test(domain)) 
+            {
+            errorMessage.push(' Email Address given does not  have a valid prefix or domain'); 
+            }
+            const phoneValidationResult = validatePhone(phone);
+            if(phoneValidationResult !== true){
+                errorMessage.push(phoneValidationResult);
+            }
+            if(errorMessage.length>0){
+                event.preventDefault();
+                errorDiv.innerHTML = errorMessage.join('<br>');
+                errorDiv.style.display ='block';
+            }
+        });
+    }
+
+    if(resetForm){
+        resetForm.addEventListener('submit', function(event){
+            let errorMessage =[];
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            const confirmPassword = document.getElementById('confirmPassword').value;
+            if(!validateEmail(email)){
+                errorMessage.push('Please enter a valid email address.');
+            }
+            const [prefix,domain]=email.split("@");
+            if(!/^([a-zA-Z0-9]+([_\.-]?[a-zA-Z0-9]+)*)$/.test(prefix) || !/^([a-zA-Z0-9-]+)+(\.[a-zA-Z]{2,})+$/.test(domain)) 
+            {
+            errorMessage.push(' Email Address given does not  have a valid prefix or domain'); 
+            }
+            if(!password){
+                errorMessage.push('Please enter your password');               
+            }
+            if(!confirmPassword){
+                errorMessage.push('Please enter your Confirm password');               
+            }
+            if(password && confirmPassword){
+                if(password !== confirmPassword){
+                    errorMessage.push(`Password and Confirm password don't match.`)
+                }
+                if(typeof(password) !== 'string' || password.includes(' ')|| password.length<8){
+                    errorMessage.push('Password must be a valid string with no spaces and should be at least of 8 characters long.');
+                }
+                if(!/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[^A-Za-z0-9]/.test(password)){
+                    errorMessage.push('Password must contain at least one uppercase character, one number and one special character.');
                 }
                 if(typeof(confirmPassword) !== 'string' || confirmPassword.includes(' ')|| confirmPassword.length<8){
                     errorMessage.push('ConfirmPassword must be a valid string with no spaces and should be at least of 8 characters long.');
@@ -53,6 +209,11 @@ document.addEventListener("DOMContentLoaded", function(){
             if(!validateEmail(email)){
                 errorMessage.push('Please enter a valid email address.');
             }
+            const [prefix,domain]=email.split("@");
+            if(!/^([a-zA-Z0-9]+([_\.-]?[a-zA-Z0-9]+)*)$/.test(prefix) || !/^([a-zA-Z0-9-]+)+(\.[a-zA-Z]{2,})+$/.test(domain)) 
+            {
+            errorMessage.push(' Email Address given does not  have a valid prefix or domain'); 
+            }
             if(!password){
                 errorMessage.push('Please enter your password');               
             }
@@ -61,7 +222,7 @@ document.addEventListener("DOMContentLoaded", function(){
                     errorMessage.push('Password must be a valid string with no spaces and should be at least of 8 characters long.');
                 }
                 if(!/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[^A-Za-z0-9]/.test(password)){
-                    errorMessage.push('Password must be contain at least one uppercase character, one number and one special character.');
+                    errorMessage.push('Password must contain at least one uppercase character, one number and one special character.');
                 }
             }
             if(errorMessage.length>0){
@@ -91,6 +252,11 @@ document.addEventListener("DOMContentLoaded", function(){
             if(!validateEmail(email)){
                 errorMessage.push('Please enter a valid email address.');
             }
+            const [prefix,domain]=email.split("@");
+            if(!/^([a-zA-Z0-9]+([_\.-]?[a-zA-Z0-9]+)*)$/.test(prefix) || !/^([a-zA-Z0-9-]+)+(\.[a-zA-Z]{2,})+$/.test(domain)) 
+            {
+            errorMessage.push(' Email Address given does not  have a valid prefix or domain'); 
+            }
             const phoneValidationResult = validatePhone(phone);
             if(phoneValidationResult !== true){
                 errorMessage.push(phoneValidationResult);
@@ -106,7 +272,7 @@ document.addEventListener("DOMContentLoaded", function(){
                     errorMessage.push('Password must be a valid string with no spaces and should be at least of 8 characters long.');
                 }
                 if(!/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[^A-Za-z0-9]/.test(password)){
-                    errorMessage.push('Password must be contain at least one uppercase character, one number and one special character.');
+                    errorMessage.push('Password must contain at least one uppercase character, one number and one special character.');
                 }
                 if(typeof(confirmPassword) !== 'string' || confirmPassword.includes(' ')|| confirmPassword.length<8){
                     errorMessage.push('ConfirmPassword must be a valid string with no spaces and should be at least of 8 characters long.');
