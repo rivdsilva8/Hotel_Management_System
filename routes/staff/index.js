f7606f0cebd7a8383d4db1cc196b451ca1f6562a
sbahala <112838583+sbahala@users.noreply.github.com>
2023-12-16 21:51:21 -0500

    Added staff route codes for basic page routing

--- routes/staff/index.js
+++ routes/staff/index.js
@@ -6,6 +6,21 @@ import markRoutes from './markDirtyRoom.js'
 
 const router = Router();
 
+router.get("/", async (req, res) => {
+    if(!req.session.user){
+        return res.redirect('/login');
+    }
+  try {
+    res.render("../views/staff/staffDashboard", {
+      title: "Welcome to Staff Page",
+      userId:req.session.user.id
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 router.use('/checkIn', checkInRoutes);
 router.use('/checkOut', checkOutRoutes);
 router.use('/markDirtyRoom', markRoutes)
