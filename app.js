ed5327e5b5e718511c9bf00b5219d65c80eeae5a
rivdsilva8 <rivaldodsilva8@gmail.com>
2023-12-17 00:08:15 -0500

    added middleware, pending logout

--- app.js
+++ app.js
@@ -16,20 +16,160 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    name: 'AuthState',
+    name: "AuthState",
     secret: "secret session",
     resave: false,
     saveUninitialized: false,
-    cookie:{maxAge: 30*60*1000}
+    cookie: { maxAge: 30 * 60 * 1000 },
     //cookie: { secure: false },
   })
 );
-app.engine("handlebars", exphbs.engine({ defaultLayout: "main",
-helpers:{eq:(v1,v2)=>v1===v2,
-  isSelected:function(code,phonePrefix){
-    return code === phonePrefix ?'selected ="selected"':'';
-  }} }));
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "main",
+    helpers: {
+      eq: (v1, v2) => v1 === v2,
+      isSelected: function (code, phonePrefix) {
+        return code === phonePrefix ? 'selected ="selected"' : "";
+      },
+    },
+  })
+);
 app.set("view engine", "handlebars");
+
+//if user is admin, never let them see login and register
+app.use("/login", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role === "admin") {
+      return res.redirect("/admin");
+    }
+  }
+  next();
+});
+
+app.use("/register", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role === "admin") {
+      return res.redirect("/admin");
+    }
+  }
+  next();
+});
+
+//if user is staff, never let them see login and register
+app.use("/login", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role === "staff") {
+      return res.redirect("/staff");
+    }
+  }
+  next();
+});
+
+app.use("/register", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role === "staff") {
+      return res.redirect("/staff");
+    }
+  }
+  next();
+});
+
+//if user is guest, never let them see login and register
+app.use("/login", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role === "user") {
+      return res.redirect("/guest");
+    }
+  }
+  next();
+});
+
+app.use("/register", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role === "user") {
+      return res.redirect("/guest");
+    }
+  }
+  next();
+});
+
+//if you are not an admin send to homescreen
+app.use("/admin", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role != "admin") {
+      console.log("you are not a admin! redirecting to home");
+      return res.redirect("/");
+    }
+  }
+  next();
+});
+//if you are not a staff send to homescreen
+app.use("/staff", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role != "staff") {
+      console.log("you are not a staff! redirecting to home");
+      return res.redirect("/");
+    }
+  }
+  next();
+});
+
+//if you are not a guest send to homescreen
+app.use("/guest", (req, res, next) => {
+  if (req.session.user) {
+    if (req.session.user.role != "user") {
+      console.log("you are not a guest! redirecting to home");
+      return res.redirect("/");
+    }
+  }
+  next();
+});
+
+//if trying to access guest,admin or staff unauthenticated - send back to login
+app.use("/admin", (req, res, next) => {
+  if (!req.session.user) {
+    console.log("you are not an admin! redirecting to login");
+    return res.redirect("/login");
+  }
+  next();
+});
+
+app.use("/staff", (req, res, next) => {
+  if (!req.session.user) {
+    console.log("you are not a staff! redirecting to login");
+    return res.redirect("/login");
+  }
+  next();
+});
+
+app.use("/guest", (req, res, next) => {
+  if (!req.session.user) {
+    console.log("you are not a guest! redirecting to login");
+    return res.redirect("/login");
+  }
+  next();
+});
+
+//middleware to log all requests
+app.use("/", (req, res, next) => {
+  if (req.originalUrl === "/favicon.ico") {
+    return next();
+  }
+
+  if (req.originalUrl.startsWith("/public")) {
+    return next();
+  }
+
+  console.log(
+    `[${new Date().toUTCString()}]: ${req.method} ${req.originalUrl} (${
+      req.session.user ? "Authenticated User" : "Non-Authenticated User"
+    })`
+  );
+
+  next();
+});
 
 configRoutes(app);
 
