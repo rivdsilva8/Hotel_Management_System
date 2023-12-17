b39f44dfae6944befecffb258ae5f8fbe60f51c3
mpisharo <mpisharo@stevens.edu>
2023-12-17 17:15:42 -0500

    Added core function cleaned code

--- public/js/BookingAjax.js
+++ public/js/BookingAjax.js
@@ -1,69 +1,69 @@
 (function ($) {
-    let mySearchForm = $('#BookingGetData'),
-      firstNameInput = $('#DisplayFirstName'),
-      emailInput = $('#DisplayEmailId'),
-      searchResults = $('#search-results'),
-      error = $('#error'),
-      empty = $('#empty');
-  
-    mySearchForm.submit(async (event) => {
-      event.preventDefault();
-      $('#noResults').hide();
-      error.hide();
-  
-      let firstName = firstNameInput.val();
-      let email = emailInput.val();
-  
-      if (firstName) {
-        if (firstName.trim().length === 0) {
-          searchResults.hide();
-          empty.hide();
-          error.show();
-          error.html("Error: First Name Can not contain empty spaces");
-          return;
-        }
-      }
+  let mySearchForm = $('#BookingGetData'),
+    firstNameInput = $('#DisplayFirstName'),
+    emailInput = $('#DisplayEmailId'),
+    searchResults = $('#search-results'),
+    error = $('#error'),
+    empty = $('#empty');
 
-      if (email) {
-        if (email.trim().length === 0) {
-          searchResults.hide();
-          empty.hide();
-          error.show();
-          error.html("Error: Email Can not contain empty spaces");
-          return;
-        }
-      }
-  
-      if (firstName && email) {
-  
-        let requestConfig = {
-          method: 'POST',
-          url: '/admin/booking/search.html',
-          contentType: 'application/json',
-          data: JSON.stringify({
-            firstName: firstName,
-            email: email
-          })
-        };
-  
-        $.ajax(requestConfig).then(function (responseMessage) {
-          let newElement = $(responseMessage);
-          if (newElement.children().length === 0) {
-            empty.hide();
-            searchResults.show();
-            searchResults.empty();
-            $('#noResults').show();
-            $('#noResults').html("No results found. Please try again.");
-          }
+  mySearchForm.submit(async (event) => {
+    event.preventDefault();
+    $('#noResults').hide();
+    error.hide();
 
+    let firstName = firstNameInput.val();
+    let email = emailInput.val();
+
+    if (firstName) {
+      if (firstName.trim().length === 0) {
+        searchResults.hide();
+        empty.hide();
+        error.show();
+        error.html("Error: First Name Can not contain empty spaces");
+        return;
+      }
+    }
+
+    if (email) {
+      if (email.trim().length === 0) {
+        searchResults.hide();
+        empty.hide();
+        error.show();
+        error.html("Error: Email Can not contain empty spaces");
+        return;
+      }
+    }
+
+    if (firstName && email) {
+
+      let requestConfig = {
+        method: 'POST',
+        url: '/admin/booking/search.html',
+        contentType: 'application/json',
+        data: JSON.stringify({
+          firstName: firstName,
+          email: email
+        })
+      };
+
+      $.ajax(requestConfig).then(function (responseMessage) {
+        let newElement = $(responseMessage);
+        if (newElement.children().length === 0) {
           empty.hide();
           searchResults.show();
           searchResults.empty();
-          searchResults.html(newElement);
-        });
-      } else {
-        empty.show();
-        searchResults.hide();
-      }
-    });
-  })(window.jQuery);
+          $('#noResults').show();
+          $('#noResults').html("No results found. Please try again.");
+        }
+
+        empty.hide();
+        searchResults.show();
+        searchResults.empty();
+        searchResults.html(newElement);
+      });
+    } else {
+      empty.show();
+      searchResults.hide();
+    }
+  });
+})(window.jQuery);
