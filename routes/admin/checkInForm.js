2dc8d5279125d7187dfd0b2ce4746f53614563b7
mpisharo <mpisharo@stevens.edu>
2023-12-18 21:34:40 -0500

    Added Validation for Core Functions

--- routes/admin/checkInForm.js
+++ routes/admin/checkInForm.js
@@ -1,35 +1,54 @@
 import { Router } from "express";
 import * as checkInDetail from '../../data/checkedinandout.js'
 
-
 const router = Router();
 
-router.route('/').get((req,res)=>{
-    return res.render('./Admin/adminCheckinandout/adminCheckin',{title:"Check In Page"});
-})
+router.route('/').get((req, res) => {
+    try {
+        return res.render('./Admin/adminCheckinandout/adminCheckin', { title: "Check In Page" });
+    } catch (e) {
+        return res.status(404).render('error', { title: 'Error', error: e });
+    }
+});
 
-router.route('/getbookingbyemail/:email').get(async (req,res)=>{
-    let GetBookingEmail = await checkInDetail.getBookingbyEmail(req.params.email)
-    if (GetBookingEmail === "-1") return res.json({})
-    return res.json(GetBookingEmail)
-})
+router.route('/getbookingbyemail/:email').get(async (req, res) => {
+    try {
+        let GetBookingEmail = await checkInDetail.getBookingbyEmail(req.params.email);
+        if (GetBookingEmail === "-1") return res.json({});
+        return res.json(GetBookingEmail);
+    } catch (e) {
+        return res.status(404).render('error', { title: 'Error', error: e });
+    }
+});
 
-router.route('/clean/:emailId').post(async (req,res) =>{
-    let AddCheckInEmail = await checkInDetail.clean(req.params.emailId)
-    if (AddCheckInEmail === "-1") return res.json({})
-    return res.json(AddCheckInEmail)
-})
+router.route('/putCheckIne/:emailId').post(async (req, res) => {
+    try {
+        let AddCheckInEmail = await checkInDetail.putCheckIne(req.params.emailId);
+        if (AddCheckInEmail === "-1") return res.json({});
+        return res.json(AddCheckInEmail);
+    } catch (e) {
+        return res.status(404).render('error', { title: 'Error', error: e });
+    }
+});
 
-router.route('/putCheckIne/:emailId').post(async (req,res) =>{
-    let AddCheckInEmail = await checkInDetail.putCheckIne(req.params.emailId)
-    if (AddCheckInEmail === "-1") return res.json({})
-    return res.json(AddCheckInEmail)
-})
+router.route("/makeBooking/:emailId").post(async (req, res) => {
+    try {
+        let MakeBookingEmail = await checkInDetail.makeBooking(req.params.emailId);
+        if (MakeBookingEmail == "-1") return res.json({});
+        return res.json(MakeBookingEmail);
+    } catch (e) {
+        return res.status(404).render('error', { title: 'Error', error: e });
+    }
+});
 
-router.route("/makeBooking/:emailId").post(async (req,res) => {
-    let MakeBookingEmail = await checkInDetail.makeBooking(req.params.emailId)
-    if (MakeBookingEmail == "-1") return res.json({})
-    return res.json(MakeBookingEmail)
-})
+router.route('/clean/:emailId').post(async (req, res) => {
+    try {
+        let AddCheckInEmail = await checkInDetail.clean(req.params.emailId);
+        if (AddCheckInEmail === "-1") return res.json({});
+        return res.json(AddCheckInEmail);
+    } catch (e) {
+        return res.status(404).render('error', { title: 'Error', error: e });
+    }
+});
 
-export default router
+export default router;
